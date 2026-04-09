dba083a4e1bd35087c190a5ec9f031d1fe0bb7be
Kasper Bankler <kasperbankler@gmail.com>
2026-04-09 20:23:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import ollama
 response = ollama.chat(model='gemma4:e4b', messages=[
     {
         'role': 'user',
-        'content': 'Give me a good name for a github repository where i explore Gemma4. I am thinking of "Gemma4Playground".',
-        # 'content': 'Write a short and clever Python script to reverse a string.',
+        'content': 'Write a short and clever Python script to reverse a string.',
     },
 ])
 
